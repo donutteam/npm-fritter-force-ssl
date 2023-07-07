0da180c8aa56b56929a33e257f69b6e673f351d8
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-07-06 22:08:13 -0400

    Initial commit.

---
+++ src/classes/FritterForceSSLMiddleware.ts
@@ -0,0 +1,35 @@
+//
+// Imports
+//
+
+import { isLocalIpAddress } from "@donutteam/string-utilities";
+import { FritterMiddlewareFunction } from "@fritter/core";
+
+//
+// Class
+//
+
+/** A class for creating a middleware that forces SSL. */
+export class FritterForceSSLMiddleware
+{
+	/** The middleware function. */
+	public readonly execute : FritterMiddlewareFunction;
+
+	/** Creates a new instance of the middleware. */
+	constructor()
+	{
+		this.execute = async (context, next) =>
+		{
+			if (context.fritterRequest.isSecure() || isLocalIpAddress(context.fritterRequest.getIp()))
+			{
+				return await next();
+			}
+
+			const url = new URL(context.fritterRequest.getUrl());
+
+			url.protocol = "https:";
+
+			context.fritterResponse.setRedirect(url.toString());
+		};
+	}
+}
